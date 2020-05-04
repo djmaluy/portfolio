9ad019d1c76fbaf3f5f33e943be3f943599389da
Andrii Tsvirko <andrii.tsvirko@gmail.com>
2020-05-04 19:54:17 +0200

    uncoment lines in index.js

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 
-// const myButton = document.getElementById('contacts-btn');
-//
-// const myButtonListener = () => {
-//
-//   let div = document.createElement('div');
-//   div.innerHTML = "какой-то текст";
-//
-//   document.body.appendChild(div);
-//
-// };
-//
-// myButton.addEventListener('click', myButtonListener);
+const myButton = document.getElementById('contacts-btn');
+
+const myButtonListener = () => {
+
+  let div = document.createElement('div');
+  div.innerHTML = "какой-то текст";
+
+  document.body.appendChild(div);
+
+};
+
+myButton.addEventListener('click', myButtonListener);
 
 
 
