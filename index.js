9d561a692e9b37bca09ff334b07396ed7aff437e
Andrii Tsvirko <andrii.tsvirko@gmail.com>
2020-05-09 10:13:35 +0200

    Added social icons

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const myButtonClick = document.getElementById('contacts-btn');
 let AddNewElementWithText = () => {
   const newElement = document.createElement('div');
-  newElement.innerText = 'How can i do this?';
+  newElement.innerText = 'How did i do this?';
+  newElement.style = 'font-size: 30px; color: red';
   document.body.appendChild(newElement);
 }
 myButtonClick.addEventListener('click', AddNewElementWithText);
