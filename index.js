2a6317d860f8d797d6a2b0608aad1ce554778cb9
Andrii Tsvirko <andrii.tsvirko@gmail.com>
2020-05-15 08:41:09 +0200

    Added <sections> and <articles> for semantics. Fixed errors

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const myButtonClick = document.getElementById('contacts-btn');
-let AddNewElementWithText = () => {
+let addNewElementWithText = () => {
   const newElement = document.createElement('div');
   newElement.innerText = 'How did i do this?';
   newElement.style = 'font-size: 30px; color: red';
   document.body.appendChild(newElement);
 }
-myButtonClick.addEventListener('click', AddNewElementWithText);
+myButtonClick.addEventListener('click', addNewElementWithText);
 
+
+let clickButtonToShowMyName = document.getElementById("myButton");
+let showMyNameByHover = () => {
+  console.log("Andrii");
+}
+let showMyNameByClick = () => {
+  alert('Andrii')
+}
+clickButtonToShowMyName.addEventListener('click', showMyNameByClick);
+clickButtonToShowMyName.addEventListener('mousemove', showMyNameByHover );
 
 
 
